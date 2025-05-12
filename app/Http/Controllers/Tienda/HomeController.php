3df851c355764c65bbe84ec1596a2e2e29bee79d
luisvilca10 <martinvilca@upeu.edu.pe>
2025-05-12 12:01:41 -0500

    Mejoras

--- app/Http/Controllers/Tienda/HomeController.php
+++ app/Http/Controllers/Tienda/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Tienda\HomeCollection;
 use App\Models\Cottage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,34 @@ class HomeController extends Controller
         $cottages = Cottage::all();
         return response()->json([
             'cottages' => new HomeCollection($cottages),
+        ]);
+    }
+
+    public function Search(Request $request)
+    {
+        $validator = Validator::make(
+            request()->all(),
+            [
+                'check_in' => 'required|date|after_or_equal:today',
+                'check_out' => 'required|date|after:check_in',
+                'adultos' => 'required|integer|min:1',
+                'ninos' => 'required|integer|min:0',
+                'habitaciones' => 'required|integer|min:1|max:4',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        if (($request->adultos + $request->ninos) > 16) {
+            return response()->json([
+                'message' => 'El total de personas (adultos + niños) no puede ser mayor a 16.'
+            ], 422);
+        }
+
+        return response()->json([
+            'Hola Mundo',
         ]);
     }
 }
