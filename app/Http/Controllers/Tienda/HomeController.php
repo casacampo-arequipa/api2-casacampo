0b608ca305411fb3a287e1b6b503336a8d7a5f2e
luisvilca10 <martinvilca@upeu.edu.pe>
2025-05-18 23:47:02 -0500

    RecomendDates

--- app/Http/Controllers/Tienda/HomeController.php
+++ app/Http/Controllers/Tienda/HomeController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\Tienda\HomeCollection;
 use App\Models\Cottage;
 use App\Models\Package;
 use App\Models\Reservation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -25,25 +26,15 @@ class HomeController extends Controller
 
     public function search(Request $request)
     {
-        $validator = Validator::make(
-            request()->all(),
-            [
-                'check_in' => 'required|date|after_or_equal:today',
-                'check_out' => 'required|date|after:check_in',
-                'persons' => 'required|integer|min:1|max:16',
-                // 'ninos' => 'required|integer|min:0',
-                // 'habitaciones' => 'required|integer|min:1|max:4',
-            ]
-        );
+        $validator = Validator::make($request->all(), [
+            'check_in' => 'required|date|after_or_equal:today',
+            'check_out' => 'required|date|after:check_in',
+            'persons' => 'required|integer|min:1|max:16',
+        ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
-        if ($request->persons > 16) {
-            return response()->json([
-                'message' => 'El total de personas (adultos + niños) no puede ser mayor a 16.'
-            ], 422);
-        };
 
         $reservationIds = Reservation::where(function ($query) use ($request) {
             $query->whereBetween('date_start', [$request->check_in, $request->check_out])
@@ -54,27 +45,79 @@ class HomeController extends Controller
                 });
         })->pluck('id');
 
-
-
         $reservedCottageIds = DB::table('cottage_reservation')
             ->whereIn('reservation_id', $reservationIds)
             ->pluck('cottage_id');
 
-        $availableCottages = Cottage::whereNotIn('id', $reservedCottageIds)->get();
-        $availableCottageIds = $availableCottages->pluck('id')->toArray();
+        $availableCottageIds = Cottage::whereNotIn('id', $reservedCottageIds)
+            ->pluck('id')
+            ->toArray();
 
-        $packages = Package::where('max_person', '>=', $request->persons)
-            ->whereHas('cottages', function ($query) use ($availableCottageIds) {
-                $query->whereIn('cottages.id', $availableCottageIds);
-            })
-            ->with(['cottages' => function ($query) use ($availableCottageIds) {
-                $query->whereIn('cottages.id', $availableCottageIds);
-            }])
+        $package = Package::where('max_person', '>=', $request->persons)
+            ->whereHas('cottages', fn($query) => $query->whereIn('cottages.id', $availableCottageIds))
+            ->with(['cottages' => fn($query) => $query->whereIn('cottages.id', $availableCottageIds)])
             ->orderBy('max_person', 'asc')
             ->first();
-            
+
+        if ($package) {
+            return response()->json([
+                'data' => $package,
+                'message' => 'Paquete disponible en las fechas seleccionadas.'
+            ]);
+        }
+
+        // Sugerencias si no hay disponibilidad
+        $nights = Carbon::parse($request->check_in)->diffInDays(Carbon::parse($request->check_out));
+        $packageIds = Package::where('max_person', '>=', $request->persons)->pluck('id');
+
+
+
+        $suggestions = [];
+        $maxAttempts = 30;
+
+        foreach ($packageIds as $packageId) {
+            $cottageIds = DB::table('cottage_packages')
+                ->where('package_id', $packageId)
+                ->pluck('cottage_id');
+
+            for ($i = 1; $i <= $maxAttempts && count($suggestions) < 3; $i++) {
+                $start = Carbon::parse($request->check_in)->addDays($i);
+                $end = $start->copy()->addDays($nights);
+
+                $conflicts = Reservation::where(function ($query) use ($start, $end) {
+                    $query->whereBetween('date_start', [$start, $end])
+                        ->orWhereBetween('date_end', [$start, $end])
+                        ->orWhere(function ($query) use ($start, $end) {
+                            $query->where('date_start', '<=', $start)
+                                ->where('date_end', '>=', $end);
+                        });
+                })->pluck('id');
+
+                $occupied = DB::table('cottage_reservation')
+                    ->whereIn('reservation_id', $conflicts)
+                    ->pluck('cottage_id');
+
+                $available = array_diff($cottageIds->toArray(), $occupied->toArray());
+                $allPackages = Package::whereIn('id', $packageIds)
+                    ->with(['cottages' => fn($query) => $query->whereIn('cottages.id', $available)])
+                    ->get()
+                    ->keyBy('id');
+                if (count($available) > 0) {
+                    $packageInfo = $allPackages[$packageId];
+
+                    $suggestions[] = [
+                        'package_name' => $packageInfo,
+                        'start_date' => $start->toDateString(),
+                        'end_date' => $end->toDateString(),
+                    ];
+                }
+            }
+        }
+
         return response()->json([
-            'data' => $packages
+            'data' => null,
+            'message' => 'No hay disponibilidad en las fechas seleccionadas.',
+            'suggestions' => $suggestions
         ]);
     }
 }
